3a29e4455c3ceba5f8f0c0b389b96cb448314a80
Fernanda Dagostin <33377159+fernandadagostin@users.noreply.github.com>
2020-09-01 21:28:24 -0400

    working on tests

--- app.py
+++ app.py
@@ -1,18 +1,67 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 
+app.secret_key = "my precious"
+app.database = "sample.db"
 
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# use decorator to link the function to a url
 @app.route('/')
+@login_required
 def home():
-    return 'hello world!'
+    #return 'hello world!'
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template("index.html", posts=posts)
 
 
 @app.route('/welcome')
 def welcome():
     return render_template("welcome.html")
 
+
+#Get is the default method - just show info
+@app.route('/login', methods=['GET', 'POST'])  #Post method is used to send info
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error= 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were just logged in!')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out!')
+    return redirect(url_for('welcome'))
+
+
+def connect_db():
+    return sqlite3.connect(app.database)
 
 if __name__ == '__main__':
     app.run(debug=True)
